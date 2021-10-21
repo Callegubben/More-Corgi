8afc309a45227352e225bb99c13f9027e51a0d36
Calle Turesson <37700533+Callegubben@users.noreply.github.com>
2021-10-21 12:02:51 +0200

    Hook shooting towards mouse

--- Assets/Scripts/HookshotAbility.cs
+++ Assets/Scripts/HookshotAbility.cs
@@ -7,12 +7,18 @@ using UnityEngine;
 
 public class HookshotAbility : CharacterAbility
 {
+    public GameObject Hook;
+
     [SerializeField] private Vector2 _facingDirection;
 
     [SerializeField] private Vector2 _targetHitPoint;
+    [SerializeField] private Vector2 playerToMouse;
+    [SerializeField] private Vector3 _mousePosition;
 
+    public float Speed = 10f;
+    public float Offset = 0.85f;
+    public float Distance = 100f;
     public LayerMask ValidHitLayer;
-    private float Distance = 100f;
 
     protected override void Initialization()
     {
@@ -29,6 +35,9 @@ public class HookshotAbility : CharacterAbility
     public override void ProcessAbility()
     {
         base.ProcessAbility();
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        _mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
 
         var shootButtonState = _inputManager.ShootButton.State;
 
@@ -46,8 +55,9 @@ public class HookshotAbility : CharacterAbility
     private void Aim()
     {
         _facingDirection = _character.IsFacingRight ? Vector2.right : Vector2.left;
+        playerToMouse = (_mousePosition - transform.position).normalized;
 
-        RaycastHit2D raycastHit = MMDebug.RayCast(transform.position, _facingDirection, Distance, ValidHitLayer, Color.red, true);
+        RaycastHit2D raycastHit = MMDebug.RayCast(transform.position, playerToMouse, Distance, ValidHitLayer, Color.red, true);
 
         if (raycastHit)
         {
@@ -57,6 +67,25 @@ public class HookshotAbility : CharacterAbility
     }
     private void Shoot()
     {
-        
+        //_controller.GravityActive(false);
+        StopCoroutine(MoveToTarget());
+        StartCoroutine(MoveToTarget());
+        //_controller.GravityActive(true);
+    }
+
+    IEnumerator MoveToTarget()
+    {
+        float elapsedTime = 0f;
+        Vector2 targetPosition = _targetHitPoint - playerToMouse * Offset;
+        while (elapsedTime < Speed)
+        {
+            transform.position = Vector2.Lerp(transform.position , targetPosition, elapsedTime/Speed);
+            if (Vector2.Distance(targetPosition,transform.position) < 0.5f)
+            {
+                break;
+            }
+            elapsedTime += Speed * Time.deltaTime;
+            yield return null;  
+        }
     }
 }
